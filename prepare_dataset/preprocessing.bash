510ee7bbd906e2b6b18d425232eb82ea27856c0f
zxu <zxu>
2022-10-23 15:21:44 +0800

    update dataset link

--- prepare_dataset/preprocessing.bash
+++ prepare_dataset/preprocessing.bash
@@ -1,14 +1,15 @@
-# # Download sat2graph dataset
-# wget https://mapster.csail.mit.edu/sat2graph/data.zip
-# unzip data.zip
-# rm -rf data.zip 
-# mv ./data ../
+# Download sat2graph dataset
+# # This link is invalid now: wget https://mapster.csail.mit.edu/sat2graph/data.zip
+gdown https://drive.google.com/uc?id=1R8sI1RmFe3rUfWMQaOfsYlBDHpQxFH-H
+unzip data.zip
+rm -rf data.zip 
+mv ./data ../
 
-# # Generate label
-# echo "Generating labels ..."
-# python create_label.py
-# python data_split.py
-# echo "Finsh generating labels!"
+# Generate label
+echo "Generating labels ..."
+python3 create_label.py
+python3 data_split.py
+echo "Finsh generating labels!"
 
 # Get pretrained checkpoints
 gdown https://drive.google.com/uc?id=1L2NWUJlFkS1vyqhbzY33fjjAHRi-5189
