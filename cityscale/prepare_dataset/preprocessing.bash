c016e479718b82d7cdb2f70e196e2c940c474cb7
Zhenhua Xu <tonyxuhit@gmail.com>
2026-02-25 19:22:50 +0800

    Update preprocessing.bash

--- cityscale/prepare_dataset/preprocessing.bash
+++ cityscale/prepare_dataset/preprocessing.bash
@@ -1,7 +1,8 @@
 # Download sat2graph dataset
 # # This link is invalid now: wget https://mapster.csail.mit.edu/sat2graph/data.zip
 # if you cannot download this by script, download it manually at https://drive.google.com/file/d/1R8sI1RmFe3rUfWMQaOfsYlBDHpQxFH-H/view?usp=share_link
-gdown https://drive.google.com/uc?id=1R8sI1RmFe3rUfWMQaOfsYlBDHpQxFH-H
+# gdown https://drive.google.com/uc?id=1R8sI1RmFe3rUfWMQaOfsYlBDHpQxFH-H
+# !!!!!!!!!1 Google Drive links have failed, download it manually from https://cloud.tsinghua.edu.cn/d/d32cb7d4b19046ed9a42/ !!!!!!!!!!!!!
 unzip data.zip
 rm -rf data.zip 
 mkdir -p ../data
@@ -15,7 +16,8 @@ echo "Finsh generating labels!"
 
 # Get pretrained checkpoints
 # if you cannot download this by script, download it manually at https://drive.google.com/file/d/1AwlFt06GRJaHKk1rboATKjv2qK5LnEvn/view?usp=share_link
-gdown https://drive.google.com/uc?id=1AwlFt06GRJaHKk1rboATKjv2qK5LnEvn
+# gdown https://drive.google.com/uc?id=1AwlFt06GRJaHKk1rboATKjv2qK5LnEvn
+# !!!!!!!!!1 Google Drive links have failed, download it manually from https://cloud.tsinghua.edu.cn/d/d32cb7d4b19046ed9a42/ !!!!!!!!!!!!!
 unzip pretrain_cityscale.zip
 rm -rf pretrain_cityscale.zip 
 
